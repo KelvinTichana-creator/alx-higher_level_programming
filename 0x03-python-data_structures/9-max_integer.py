e160f6dcd976610077cc2f6034cadf33a4297b68
Kelvin Carrington Tichana <kelvin.tichana@ashesi.edu.gh>
2024-01-22 15:56:55 -0800

    further checks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    else:
-        maxy = my_list[0]
-        for i in my_list:
-            if i > maxy:
-                maxy = i
-        return maxy
+    maxy = my_list[0]
+    for i in my_list:
+        if i > maxy:
+            maxy = i
+    return maxy
 
